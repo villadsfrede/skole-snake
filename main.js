a7199abb72c3a4c96ee1d2efe9941e563bdb5a76
Villads Frederiksen <villadsfrederiksen1@gmail.com>
2023-01-11 15:11:38 +0100

    Added div

--- main.js
+++ main.js
@@ -8,4 +8,11 @@ var gridSize = 25
 window.addEventListener("load", () => {
     canvas.width = x*gridSize;
     canvas.height = y*gridSize;
+
+    ctx.beginPath()
+    ctx.rect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = "blue";
+    ctx.fill()
+    ctx.closePath();
 });
+
